ecf300683ce1d3add430f63b8cb6e38266d783f6
Lee Kuan Yong <kylee.2014@smu.edu.sg>
2018-02-24 13:44:37 +0800

    Implemented Changes
    Implemented changes:
    - adjusted table
    - connected the flow from add to cart to checkout
    - displayed total price of all items
    - make adding of item quantity dynamic

--- public/js/controllers/CartCtrl.js
+++ public/js/controllers/CartCtrl.js
@@ -4,4 +4,24 @@ angular.module('CartCtrl', [])
         //retrieve transaction reference object to store transaction
         var transactRef = firebase.database().ref().child('Cart');
         $scope.checkoutItem = $firebaseObject(transactRef);
+
+        var checkoutItem = $firebaseArray(transactRef);
+        console.log("checkoutitem is ", checkoutItem);
+
+        //method to assign total price to the cart
+        checkoutItem.$loaded().then(function (menu) {
+            //to bind the food menu dynamically
+            //must first check for keys, then bind it to the array
+            // console.log("menu: ", menu);
+            var totalPrice = 0;
+            for (var i in menu) {
+                if (menu[i]['qty'] != undefined && menu[i]['price'] != undefined) {
+                    totalPrice += (menu[i]['qty'] * menu[i]['price'])
+                }
+
+            }
+            console.log("total price is: ", totalPrice);
+            $scope.totalPrice = totalPrice;
+        })
+
     }]);
--- public/js/controllers/SelectedItem.js
+++ public/js/controllers/SelectedItem.js
@@ -1,5 +1,5 @@
 angular.module('SelectedItemCtrl', [])
-    .controller('SelectedItemController', ['$scope', '$routeParams', '$firebaseArray', '$firebaseObject', '$firebaseAuth', '$mdDialog', 'checkOutItems', '$location', '$websocket', '$q', 'websocketService', 'sessionService', 'selectedItem', function ($scope, $routeParams, $firebaseArray, $firebaseObject, $firebaseAuth, $mdDialog, checkOutItems, $location, $websocket, $q, websocketService, sessionService, selectedItem) {
+    .controller('SelectedItemController', ['$scope', '$routeParams', '$firebaseArray', '$firebaseObject', '$firebaseAuth', '$mdDialog', 'checkOutItems', '$location', '$websocket', '$q', 'websocketService', 'sessionService', 'selectedItem', '$mdToast', function ($scope, $routeParams, $firebaseArray, $firebaseObject, $firebaseAuth, $mdDialog, checkOutItems, $location, $websocket, $q, websocketService, sessionService, selectedItem, $mdToast) {
 
         var selectedItem = selectedItem.getSelectedItem();
         console.log("selectedItem is: ", selectedItem);
@@ -7,12 +7,37 @@ angular.module('SelectedItemCtrl', [])
         $scope.dimensions = selectedItem.dimensions;
         console.log("dimensions is: ", $scope.dimensions);
 
+        var cartref = firebase.database().ref().child('Cart');
+        $scope.cartArr = $firebaseArray(cartref);
+
+        $scope.processAddToCart = function (quantity, selectedItem) {
+
+            var fashionRef = firebase.database().ref().child('Cart')
+            $scope.cartArr.$add({
+                category: "watch",
+                imageUrl: selectedItem.imageUrl,
+                name: selectedItem.name,
+                price: selectedItem.price,
+                dimensions: selectedItem.dimensions,
+                qty: quantity
+            }).then(function (ref) {
+                console.log("data added!");
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Cart Added!')
+                        .position('top right left')
+                        .hideDelay(3000)
+                        .toastClass('md-toast-done')
+                );
+                // sessionService.unset('foodList');
+                // sessionService.unset('checkBox');
+                // sessionService.unset('totalPrice');
+                $location.path('/')
+            })
+
+        }
 
         // $scope.fashionItemsSizes = selectedItem.size
-
-
-
-
 
 
     }])
